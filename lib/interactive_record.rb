3aaf1e3d35c17269b905e741ed457d39ca8221b5
Dave Winniford <dwinniford@gmail.com>
2020-02-13 18:54:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,7 @@ class InteractiveRecord
     values = []
     self.class.column_names.each do |col_name|
       if col_name != "id"
-        values << "'#{send(col_name)}'"
+        values << "'#{col_name}'"
       end 
     end 
     values.join(", ")
